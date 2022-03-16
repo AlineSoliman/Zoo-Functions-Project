53e9e4353607beb16005445a54387d7e453c3fe4
Aline Soliman <alineapreis@hotmail.com>
2022-03-16 19:26:25 -0300

    Adiciona requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const employee = {};
+  if (employeeName) {
+    return data.employees.find((element) =>
+      (element.firstName === employeeName || element.lastName === employeeName));
+  }
+  return employee;
 }
-
+console.log(getEmployeeByName('Ola'));
 module.exports = getEmployeeByName;
