8e2367919a5179a619e630004ad0db44e3c63596
Aline Soliman <alineapreis@hotmail.com>
2022-03-17 18:11:10 -0300

    adiciona requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((element) =>
+    (id === stephanieId || id === olaId || id === burlId));
 }
+console.log(isManager('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(stephanieId) === true && managerId === stephanieId) {
+    return ['Burl Bethea', 'Ola Orloff', 'Emery Elser'];
+  }
+  if (isManager() === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
 }
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
