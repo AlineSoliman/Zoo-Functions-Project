740c2f363709394ac472ca89de5f06c1a9315063
Aline Soliman <alineapreis@hotmail.com>
2022-03-17 14:40:21 -0300

    adiciona requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animals = data.species.find((element) => element.name === animal); // percorre procurando o nome
+  return animals.residents.every((element2) => element2.age >= age); // percorre por age
 }
+console.log(getAnimalsOlderThan('lions', 7));
 
 module.exports = getAnimalsOlderThan;
