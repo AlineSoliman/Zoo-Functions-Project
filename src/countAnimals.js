fdbeac6c4efeede1ac2caa304c81c04bfc147138
Aline Soliman <alineapreis@hotmail.com>
2022-03-18 15:45:48 -0300

    adiciona requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
+const specieQtd = data.species.reduce((acc, curr) => {
+  (acc[curr.name] = (curr.residents).length);
+  return acc;
+}, {});
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return specieQtd;
+  }
+  const { specie, sex } = animal; // desestruturar para abranger os 2 paramnetros em animal
+  if (sex === undefined) { // se sex não for passado como parâmentro
+    return specieQtd[specie];
+  }
+  const animals = data.species.find((element) => element.name === specie);
+  const genero = animals.residents.filter((element2) => element2.sex === sex);
+  return genero.length;
 }
+
+console.log(countAnimals({ specie: 'giraffes', sex: 'female' }));
 
 module.exports = countAnimals;
