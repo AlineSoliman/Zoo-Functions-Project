47e6a30d68ea33acf325daa84b6dca47dd869b0a
Aline Soliman <alineapreis@hotmail.com>
2022-03-20 22:39:38 -0300

    Adiciona requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,24 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const findFirstSpecie = (id) => {
+  const employee = employees.find((element) => element.id === id);
+  const responsible = employee.responsibleFor[0];// primeira espécie gerenciada
+  return responsible;
+};
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  findFirstSpecie(id);
+  const array = data.species.find((element) => element.id === findFirstSpecie(id));
+  console.log(array);
+  const ages = array.residents.reduce((acc, item) => {
+    if (acc.age > item.age) {
+      return acc;
+    }
+    return item;
+  }, []);
+  return [ages.name, ages.sex, ages.age];
 }
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
